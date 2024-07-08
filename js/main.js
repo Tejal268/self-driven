26ac9551e806f52f69a5fc9457e582f4acda63a6
Tejal268 <88198504+Tejal268@users.noreply.github.com>
2024-07-08 20:46:05 +0530

    first commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var typed=new typed(".text",{
+    Strings:["java developer","frontend developer","MySQL challenger"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+})
+
---
+++ main.js
@@ -0,0 +1,9 @@
+
+    var typed = new Typed('#text', {
+      strings: ['Java developer', 'Mysql challenger','web developer'],
+      typeSpeed: 100,
+      backspeed:100,
+      loop:true
+    });
+
+
